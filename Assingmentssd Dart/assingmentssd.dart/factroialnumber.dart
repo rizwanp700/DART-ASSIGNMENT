4fa919e0a6db0315b17efefdc301d247c676a63c
Rizwan <140622863+rizwanp700@users.noreply.github.com>
2023-09-23 12:39:39 +0530

    Add files via upload

---
+++ Assingmentssd Dart/assingmentssd.dart/factroialnumber.dart
@@ -0,0 +1,20 @@
+ //11- Write a function to calculate the factorial of a given number n
+
+void main () {
+  int n =5;
+  int factorial=1;
+
+  for (int i=1;i<=n ;i++){
+    factorial= factorial*i;
+
+
+  }
+
+  print("the factorual $n is $factorial" );
+}
+
+// 1
+// 2
+// 6
+// 24
+// 120
---
+++ Assingmentssd Dart/assingmentssd.dart/max.dart
@@ -0,0 +1,8 @@
+// 1.Write a function to find the maximum element in a array
+
+void main () {
+  List <int> numbers= [10,2,8,66,999,5,7,6];
+  numbers.sort();
+  numbers.last;
+  print(numbers.last);
+}
